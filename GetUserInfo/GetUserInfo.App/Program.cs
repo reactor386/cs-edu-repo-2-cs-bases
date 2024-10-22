95daab40b69b4dadaa3c6779344822da2578f446
reactor386 <reactor386@gmail.com>
2024-10-22 21:31:28 +0300

    Создание консольного проекта
     - добавлен консольный проект .Net 8.0
     - обновлен файл CHANGELOG.md

---
+++ GetUserInfo/GetUserInfo.App/Program.cs
@@ -0,0 +1,18 @@
+﻿// -
+using System;
+using System.Text;
+
+namespace GetUserInfo
+{
+    internal class Program
+    {
+        private static int Main (string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
+
